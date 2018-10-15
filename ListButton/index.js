038fe59db38bc8729ad2f934f8d9c17215dc3ef6
dorilusions <dorothyz.lu@gmail.com>
2018-10-14 22:43:52 -0700

    Edits to ListButton

--- ListButton/index.js
+++ ListButton/index.js
@@ -1,9 +1,17 @@
 window.onload = () => {
+    // Part I
     const userInput = document.getElementById('list-data');
     const setTextButton = document.getElementById('set-text');
     const listButton = document.getElementById('add-to-list');
 
+    // Part II
     setTextButton.onclick = () => {
         userInput.value = 'this is the first item for my list';
+    }
+
+    // Part III
+    listButton.onclick = () => {
+        const myList = document.getElementById('my-list');
+        elfCode.appendToList(myList, userInput.value);
     }
 }

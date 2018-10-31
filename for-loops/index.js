377b1cdd3794f16c0a86137271b811a7ac1c18b0
Dorothy Lu <dorothy.lu@bellevuecollege.edu>
2018-10-31 12:21:35 -0700

    in class

--- for-loops/index.js
+++ for-loops/index.js
@@ -3,9 +3,8 @@ window.onload = () => {
 
     listButton.onclick = () => {
         const myList = document.getElementById('my-list');
-            // WRAP THE NEXT LINE IN A for-loop that iterates over a variable named i. 
-            // The loop should iterate 5 times (count from zero to five).
-            elfCode.appendToList(myList, i);
-            // END LOOP
+            for(i = 1; i < 10; i++){
+                elfCode.appendToList(myList, i);
+            }
     }
 }                    

e4163fdd7958d2b304b6fedc9daecdf1010c1afc
Dorothy Lu <dorothy.lu@bellevuecollege.edu>
2018-11-05 12:17:01 -0800

    in class 11-5

--- for-loops/index.js
+++ for-loops/index.js
@@ -1,12 +1,13 @@
 window.onload = () => {
+
+    // listButton is a variable
     const listButton = document.getElementById('run-for-loop');
 
-    listButton.onclick = () => {
+    listButton.onclick = function () { // anonymous function
         const myList = document.getElementById('my-list');
         
         for (let i = 0; i < 7; i++) {
            elfCode.appendToList(myList, i);
-        }
-        
+        }       
     }
 }
---
+++ javascript-test/function-basics.js
@@ -0,0 +1,21 @@
+function simpleFunction () {
+    console.log("A function consists of statements that will perform a single task.");
+}
+
+function functionParameters (v) {
+    console.log(v);
+}
+
+function add (x, y){
+    console.log(x + y);
+}
+
+//
+function adder () {
+    console.log(2 + 5);
+}
+
+simpleFunction();
+functionParameters("Functions can take parameters");
+add(2, 5);
+adder();

0314a324ab7df621ed9f05201fb9de0d6bd822ac
Dorothy Lu <dorothy.lu@bellevuecollege.edu>
2018-11-26 12:24:39 -0800

    in class

--- javascript-test/javascript-arrays.js
+++ javascript-test/javascript-arrays.js
@@ -21,13 +21,17 @@ window.onload = () => {
         }  
     };
 
-    //techLanguagePopularityAction.onclick = () => {};
+    techLanguagePopularityAction.onclick = () => {
+        
+    };
 }
 
 const arrays = {
     numbers : [15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5],
     languageNames: ["JavaScript", "HTML", "CSS", "Java", "C#", "Python", "C/C++"],
-    //languagePopularity:
+    languagePopularity: [
+        {}
+    ]
 }
 
 for(i = 10; i >= 0; i--){

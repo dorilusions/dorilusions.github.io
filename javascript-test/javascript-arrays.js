501f71a735816c34d54c7277f0d6c167db1e73c5
dorilusions <dorothyz.lu@gmail.com>
2018-12-05 23:57:55 -0800

    final

--- javascript-test/javascript-arrays.js
+++ javascript-test/javascript-arrays.js
@@ -1,10 +1,48 @@
 window.onload = () => {
-    for(i = 10; i >= 0; i--){
-    console.log(arrays.numbers[i]);
-    }
+    
+    const numbersAction = document.getElementById('numbers-action');
+    const languageNamesAction = document.getElementById('tech-language-names-action');
+    //const techLanguagePopularityAction = document.getElementById('tech-language-popularity-action');
+
+    numbersAction.onclick = () => {
+        const numbersDisplay = document.getElementById('numbers-display');
+    
+        const showNumbers = () => {
+            for (let number of arrays.numbers) {
+                const li = document.createElement("li");
+                li.appendChild(document.createTextNode(number));
+                numbersDisplay.appendChild(li);
+            }
+        }
+    
+        showNumbers();
+        arrays.numbers.sort((a, b) => a - b);
+        showNumbers();        
+    };
+    
+    languageNamesAction.onclick = () => {
+        const languageNamesDisplay = document.getElementById('tech-language-names-display');
+        for (let language of arrays.languageNames) {
+            const lis = document.createElement("li");
+            lis.appendChild(document.createTextNode(language));
+            languageNamesDisplay.appendChild(lis);
+        }  
+    };
+
+    techLanguagePopularityAction.onclick = () => {
+
+    };
 }
 
 const arrays = {
-    numbers : [15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5]
+    numbers : [15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5],
+    languageNames: ["JavaScript", "HTML", "CSS", "Java", "C#", "Python", "C/C++"],
+    languagePopularity: [
+        {language: "HTML", rank: 1},
+        
+    ]
 }
 
+for(i = 10; i >= 0; i--){
+        console.log(arrays.numbers[i]);
+    }
